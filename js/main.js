87b17c83712f527fc67a5363ca32bb6d6372f469
Jetsuko <jessean.infiltrator@gmail.com>
2022-04-20 15:43:24 -0700

    changed format of list results to display cards.

--- js/main.js
+++ js/main.js
@@ -37,8 +37,6 @@ class App {
     }
 
     async init() {
-        //init lightbox
-        this.initLightbox();
         // set click event for button on search function
         document.querySelector("#get-cocktails").addEventListener('click', async () => {
             let ingredient = document.querySelector("input").value;
@@ -47,15 +45,22 @@ class App {
                 this.searches[ingredient] = data.drinks;
             }
             this.current = this.searches[ingredient];
-            console.log(this.current);
+            // console.log(this.current);
             // render search results
             this.renderCocktailList(this.current, 'results');
-            this.renderCocktailThumbs(this.current);
+            this.renderSkeletonCards(this.current);
+            this.fetchCocktail(this.current[0].strDrink);
         });
         // random cocktail function
         document.querySelector("#random-cocktail").addEventListener('click', async e => {
             // //fetch a random cocktail from search results
-            await this.fetchCocktail(e.currentTarget.innerText); 
+            const track = this.carouselTrack;
+            const slides = Array.from(this.carouselTrack.children);
+            const currentSlide = track.querySelector('.current-slide');
+            const drinkIndex = Math.floor(Math.random() * this.current.length);
+            const targetSlide = slides[drinkIndex];
+            await this.moveToSlide(currentSlide, targetSlide);
+            this.hideShowCarouselButtons(drinkIndex);
         });
         //add event listener to prevButton
         this.prevSlideButton.addEventListener('click', e => {
@@ -80,7 +85,7 @@ class App {
     }
 
     /**
-     * renders search results with link in list items
+     * renders text search results with link in list items
      * calls addLinkListeners on the created links
      * unhides results section if it's hidden
      * **/
@@ -104,9 +109,9 @@ class App {
         }
     }
 
-    /**
+    /** NEED TO RE-WORK EVENT LISTENER
      * collects all links in parentId
-     * addes a click listener that calls fetchCocktail
+     * adds a click listener that calls fetchCocktail
      * fetchCocktail uses the link's inner text
      * **/
     addLinkListeners(parentId) {
@@ -117,14 +122,14 @@ class App {
         // currentChoiceNumber = links.length;
         links.forEach(a => a.addEventListener('click', async e => {
             e.preventDefault();
-            console.log(e.target.innerText);
-            console.log(resultsList);
+            // console.log(e.target.innerText);
+            // console.log(resultsList);
             const currentSlide = this.carouselTrack.querySelector('.current-slide');
             const links = Array.from(resultsList.children).map(li => li.children[0]);
             const slides = Array.from(this.carouselTrack.children)
             const targetIndex = links.findIndex(listItem => listItem === e.target);
             const targetSlide = slides[targetIndex];
-            console.log(targetSlide);
+            // console.log(targetSlide);
 
             this.moveToSlide(currentSlide, targetSlide);
             this.hideShowCarouselButtons(targetIndex);
@@ -133,87 +138,34 @@ class App {
     }
 
     /**
-     * takes in a string
+     * Get and render cocktail object data.
+     *
      * filters the this.current array to find the index of a drink 
      * object that has a property name whose value matches the string
      * checks to see if drink string exists in this.cards
      * runs getFetch on string if drink does not exist in this.cards
-     * calls renderCocktail on this.cards[currentIndex]
-     * **/
+     * calls renderCocktail on this.cards[drinkName]
+     * 
+     * @param {string}  str     'Surprise me.' or a 'Drink Name'.
+     */
     async fetchCocktail(str) {
-        let drinkIndex;
         let fetch;
-        if (str === "Surprise me.") {
-            drinkIndex = Math.floor(Math.random() * this.current.length);    
-        } else {
-            drinkIndex = this.current.findIndex(drink => drink.strDrink === str);
+        if (!this.cards[str]) {
+            let drinkIndex;
+            if (str === "Surprise me.") {
+                drinkIndex = Math.floor(Math.random() * this.current.length);    
+            } else {
+                drinkIndex = this.current.findIndex(drink => drink.strDrink === str);
+            }
+            this.currentIndex = drinkIndex;
+            fetch = this.current[drinkIndex].strDrink;
+            if (!(`${fetch}` in this.cards)) {
+                const data = await this.getFetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${fetch}`);
+                const drink = data.drinks[0];
+                this.cards[drink.strDrink] = data.drinks[0];
+            }
         }
-        this.currentIndex = drinkIndex;
-        fetch = this.current[drinkIndex].strDrink;
-        if (!(`${this.currentIndex}` in this.cards)) {
-            let data = await this.getFetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${fetch}`);
-            this.cards[this.currentIndex] = data.drinks[0];
-        }
-        this.renderCocktailRecipe(this.cards[this.currentIndex]);
-    }
-
-    /**
-     * takes an object in (a drink object) and an parentElement (ul)
-     * creates list item elements for each ingredient
-     * list item is a string that shows measurement for the ingredient and the ingredient's name
-     * appends each list item to parentElement.
-     * **/
-    getIngredients(obj, parentElement) {
-        const ingredientKeys = Object.keys(obj)
-        .map(i => {
-            if (i.includes('Ingredient')) return i;
-        })
-        .filter(i => {
-            if (obj[i] !== null && obj[i] !== "") return i;
-        });
-        for (let i = 0; i < ingredientKeys.length; i++) {
-            let currentKey = ingredientKeys[i];
-            let number = currentKey.slice(currentKey.lastIndexOf('t') + 1);
-            let ingredientName = obj[currentKey];
-            let ingredientMeasure = obj[`strMeasure${number}`];
-            if (ingredientMeasure === null) ingredientMeasure = "";
-            const li = document.createElement('li');
-            li.textContent = `${ingredientMeasure} ${ingredientName}`;
-            parentElement.appendChild(li);
-        }
-    }
-
-    //render thumbs in carousel track
-    renderCocktailThumbs(arr) {
-        this.carouselTrack.textContent = '';
-        arr.forEach(drinkObj => this.renderCocktailThumb(drinkObj));
-        this.placeSlides();
-    }
-
-    //render individual thumb
-    renderCocktailThumb(obj) {
-        // create li slide item
-        console.log(obj);
-
-        const li = document.createElement('li');
-        li.classList.add('carousel__slide');
-
-        // create elements that go into slide
-
-        //name of drink
-        const name = document.createElement('h2');
-        name.classList.add('thumb-drink-name');
-        name.textContent = obj.strDrink;
-
-        //image
-        const img = document.createElement('img');
-        img.src = obj.strDrinkThumb;
-        img.classList.add('carousel__image');
-
-        // //append to ul
-        [name, img].forEach(tag => li.appendChild(tag));
-        // li.appendChild(img);
-        this.carouselTrack.appendChild(li);
+        this.renderCocktailRecipe(this.cards[str]);
     }
 
     /**
@@ -237,10 +189,11 @@ class App {
      * helper function for prev and next button clicks
      * transitions from current slide to target slide (prev or next)
      * **/
-    moveToSlide(currentSlide, targetSlide) {
+    async moveToSlide(currentSlide, targetSlide) {
         this.carouselTrack.style.transform = `translateX(-${targetSlide.style.left})`;
         currentSlide.classList.remove('current-slide');
         targetSlide.classList.add('current-slide');
+        await this.fetchCocktail(targetSlide.children[0].id);
     }
 
     /**
@@ -265,70 +218,191 @@ class App {
     }
 
     /**
-     * creates a lightbox element
-     * appends it to body
-     * adds an event listener that hides it if clicked outside of nested article
+     * Initiates the skeleton card slide rendering
+     * Calls placeSlides to place slides side-by-side
+     * sets current slide class to first slide
      * **/
-    initLightbox() {
-        const lightbox = document.createElement('div');
-        lightbox.id = 'lightbox';
-        document.body.appendChild(lightbox);
-        lightbox.addEventListener('click', e => {
-            if (e.target !== e.currentTarget) return;
-            lightbox.classList.remove('active');
-        })
+    renderSkeletonCards(arr) {
+        this.carouselTrack.textContent = '';
+        arr.forEach((obj, i) => {
+            this.renderSkeletonCard(obj.strDrink);
+        });
+        this.placeSlides();
+        document.querySelector('.carousel__slide').classList.add('current-slide');
+
+    }
+    
+    /****
+     * Render skeleton card for recipes
+     * append skeleton card to carousel__track
+     ****/
+    renderSkeletonCard(str) {
+        //helper function for adding skeleton text to an element
+        const appendSkeleText = (parent, child) => {
+            for (let i = 1; i <= 3; i++) {
+                parent.appendChild(child)
+            }
+        }
+
+        //create containing item list
+        const listItem = document.createElement('li');
+        listItem.classList.add('carousel__slide');
+
+        //create article
+        const card = document.createElement('article');
+        card.classList.add('cocktail-recipe');
+        card.setAttribute('id', str);
+
+        //create img
+        const img = document.createElement('img');
+        img.classList.add('skeleton');
+
+        //create name title
+        const nameTitle = document.createElement('h2');
+        nameTitle.setAttribute('data-name', true);
+        const nameTitleSkel = document.createElement('div');
+        nameTitleSkel.classList.add('skeleton', 'skeleton-header', 'skeleton-name');
+        nameTitle.appendChild(nameTitleSkel);
+
+        //create ingredients
+        ////title
+        const ingredientsTitle = document.createElement('h3');
+        ingredientsTitle.classList.add('ingredients-title');
+        const ingredientsTitleSkel = document.createElement('div');
+        ingredientsTitleSkel.classList.add('skeleton', 'skeleton-header');
+        ingredientsTitle.appendChild(ingredientsTitleSkel);
+        ////ingredients list
+        const ingredientsList = document.createElement('ul');
+        ingredientsList.setAttribute('data-ingredients', true);
+        //create skeleton list items for ingredientsList
+        const ingredientItem = document.createElement('li');
+        ingredientItem.classList.add('skeleton', 'skeleton-text', 'skeleton-ingredient');
+        appendSkeleText(ingredientsList, ingredientItem);
+
+        //create instructions
+        ////title
+        const instructionsTitle = document.createElement('h3');
+        instructionsTitle.classList.add('instructions-title');
+        const instructionsTitleSkel = document.createElement('div');
+        instructionsTitleSkel.classList.add('skeleton', 'skeleton-header');
+        instructionsTitle.appendChild(instructionsTitleSkel);
+        ////text
+        const instructionsText = document.createElement('p');
+        instructionsText.setAttribute('data-instructions', true);
+        //create skeleton text for instructionsText
+        const skeletonText = document.createElement('div');
+        skeletonText.classList.add('skeleton', 'skeleton-text');
+        appendSkeleText(instructionsText, skeletonText);
+        
+        //append elements to article
+        const elements = [
+            img, 
+            nameTitle, 
+            ingredientsTitle, 
+            ingredientsList, 
+            instructionsTitle, 
+            instructionsText
+        ];
+        elements.forEach(el => {
+            card.appendChild(el);
+        });
+
+        //append article to list item
+        listItem.appendChild(card);
+
+        //append list item to carousel track    
+        this.carouselTrack.appendChild(listItem);
+    }
+
+     /**
+     * finds article/cocktail-recipe by id
+     * fetches cocktail info
+     * renders cocktail info to 
+     * **/
+      renderCocktailRecipe(obj) {
+        // console.log(obj);
+        const card = this.carouselTrack.querySelector(`article[id="${obj.strDrink}"]`);
+        const img = card.querySelector('img');
+        const name = card.querySelector('[data-name]');
+        const ingredientsTitle = card.querySelector('.ingredients-title');
+        const ingredients = card.querySelector('[data-ingredients]');
+        const instructionsTitle = card.querySelector('.instructions-title');
+        const instructions = card.querySelector('[data-instructions]');
+
+        //image
+        img.src = obj.strDrinkThumb;
+
+        //name
+        name.textContent = obj.strDrink;
+
+        //ingredients
+        ingredientsTitle.textContent = 'Ingredients';
+        ingredients.textContent = '';
+        this.getIngredients(obj, ingredients);
+
+        //instructions
+        instructionsTitle.textContent = 'Instructions';
+        instructions.textContent = obj.strInstructions;
     }
 
     /**
-     * takes in a single cocktail object and renders it to the #drink choice div
-     * calls getIngredients on the passed object for help rendering the ingredient list.
-     * adds components to an article
-     * appends the article to the lightbox
-     * unhides the lightbox
+     * takes an object in (a drink object) and an parentElement (ul)
+     * creates a list item element for each ingredient
+     * list item is a string that shows measurement for the ingredient and the ingredient's name
+     * appends each list item to parentElement.
      * **/
-    renderCocktailRecipe(obj) {
-        const lightbox = document.querySelector('#lightbox');
-        lightbox.textContent = '';
-        // create article
-        console.log(obj);
-
-        const article = document.createElement('article');
-        article.classList.add('cocktail-recipe');
-
-        // create elements that go into article
-
-        //name of drink
-        const name = document.createElement('h2');
-        name.classList.add('drink-name');
-        name.textContent = obj.strDrink;
-
-        //image
-        const img = document.createElement('img');
-        img.src = obj.strDrinkThumb;
-        img.classList.add('recipe-image');
-
-        //ingredients
-        const ingredientsTitle = document.createElement('h3');
-        ingredientsTitle.textContent = 'Ingredients';
-
-        const ingredientsList = document.createElement('ul');
-        ingredientsList.classList.add('ingredients-list');
-        this.getIngredients(obj, ingredientsList);
-
-        //instructions
-        const instructionsTitle = document.createElement('h3');
-        instructionsTitle.textContent = 'Instructions';
-
-        const instructionsText = document.createElement('p');
-        instructionsText.classList.add('instructions-text');
-        instructionsText.textContent = obj.strInstructions;
-
-        // //append to ul
-        [name, img, ingredientsTitle, ingredientsList, instructionsTitle, instructionsText]
-            .forEach(tag => article.appendChild(tag));
-
-        lightbox.appendChild(article);
-        lightbox.classList.add('active');
+     getIngredients(obj, parentElement) {
+        const ingredientKeys = Object.keys(obj)
+        .map(i => {
+            if (i.includes('Ingredient')) return i;
+        })
+        .filter(i => {
+            if (obj[i] !== null && obj[i] !== "") return i;
+        });
+        for (let i = 0; i < ingredientKeys.length; i++) {
+            let currentKey = ingredientKeys[i];
+            let number = currentKey.slice(currentKey.lastIndexOf('t') + 1);
+            let ingredientName = obj[currentKey];
+            let ingredientMeasure = obj[`strMeasure${number}`];
+            if (ingredientMeasure === null) ingredientMeasure = "";
+            const li = document.createElement('li');
+            li.textContent = `${ingredientMeasure} ${ingredientName}`;
+            parentElement.appendChild(li);
+        }
     }
+
+    // //will re-purpose for rendering nav indicator dots
+    // //render thumbs in carousel track
+    // renderCocktailThumbs(arr) {
+    //     this.carouselTrack.textContent = '';
+    //     arr.forEach(drinkObj => this.renderCocktailThumb(drinkObj));
+    //     this.placeSlides();
+    // }
+
+    // //render individual thumb
+    // renderCocktailThumb(obj) {
+    //     // create li slide item
+    //     console.log(obj);
+
+    //     const li = document.createElement('li');
+    //     li.classList.add('carousel__slide');
+
+    //     // create elements that go into slide
+
+    //     //name of drink
+    //     const name = document.createElement('h2');
+    //     name.classList.add('thumb-drink-name');
+    //     name.textContent = obj.strDrink;
+
+    //     //image
+    //     const img = document.createElement('img');
+    //     img.src = obj.strDrinkThumb;
+    //     img.classList.add('carousel__image');
+
+    //     // //append to ul
+    //     [name, img].forEach(tag => li.appendChild(tag));
+    //     // li.appendChild(img);
+    //     this.carouselTrack.appendChild(li);
+    // }
 
 }
